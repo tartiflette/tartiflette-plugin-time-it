c75f8ec0a492d061a6db4c32ed16fb30eb4391c8
Aurelien Busi <aurelien.busi@numberly.com>
2021-08-05 10:59:30 +0200

    Update dependancies

--- tests/unit/test_timeit.py
+++ tests/unit/test_timeit.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_timeit_bake():
-    from tartiflette_plugin_time_it import bake, _SDL
+    from tartiflette_plugin_time_it import _SDL, bake
 
     assert await bake("a", {}) == _SDL
